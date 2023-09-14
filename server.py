a26546a1a4133d1d40a8d0ffa83eeab62e246afc
mithin-kumar <82752630+mithin-kumar@users.noreply.github.com>
2023-09-14 19:58:14 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,44 @@
-from flask import Flask
+from flask import Flask,request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    if 'first' not in data or 'second' not in data:
+        return jsonify({'error': 'Missing parameters'}), 400
+
+    num1 = data['first']
+    num2 = data['second']
+
+    try:
+        result = float(num1) + float(num2)
+        return jsonify({'result': result}), 200
+    
+    except ValueError:
+        return jsonify({'error': 'Invalid input'}), 400
+    # return ''
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    if 'second' not in data or 'first' not in data:
+        return jsonify({'error': 'Missing parameters'}), 400
+
+    num1 = data['first']
+    num2 = data['second']
+
+    try:
+        result = float(num1) - float(num2)
+        return jsonify({'result': result}), 200
+    except ValueError:
+        return jsonify({'error': 'Invalid input'}), 400
+    
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
